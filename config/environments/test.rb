37876de2c98fd80d8f715c483c967dba4dac250e
Anthony Burns <ahburns@librespeak.org>
2009-06-21 21:05:18 +0300

    Modified gem configuration in environments/test.rb to match the configuration in environments/cucumber.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,8 +27,7 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-# Gems
-config.gem 'rspec-rails', :lib => false
-config.gem 'rspec', :lib => false
-config.gem 'cucumber'
-config.gem 'webrat'
+config.gem "cucumber",    :lib => false,        :version => ">=0.3.11" unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber'))
+config.gem "webrat",      :lib => false,        :version => ">=0.4.4" unless File.directory?(File.join(Rails.root, 'vendor/plugins/webrat'))
+config.gem "rspec",       :lib => false,        :version => ">=1.2.6" unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
+config.gem "rspec-rails", :lib => 'spec/rails', :version => ">=1.2.6" unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
